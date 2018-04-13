63c5e514e2100a28a0a23bd0d02d4f92f66732f8
OGNeekoPolo <nktrabue@gmail.com>
2018-04-13 00:32:32 -0400

    Implemented UI for User Controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Meat', type: 'meat' },
+  { label: 'Cheese', type: 'cheese' }
+];
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
-
+    { controls.map(ctrl => (
+        <BuildControl label={ctrl.label} key={ctrl.label} />
+    )) }
   </div>
 );
 
